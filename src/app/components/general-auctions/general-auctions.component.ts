ae9be28e56b4e276f0b0ac92f94c43752bc2500a
Alejandro Sanchez <asanchez.90.18@gmail.com>
2026-08-03 13:09:09 -0600

    cambios en el home de subastas generales, icono, datos optimizados, iniciar subastas sin el scroll y mandara directo al home

--- src/app/components/general-auctions/general-auctions.component.ts
+++ src/app/components/general-auctions/general-auctions.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Output, EventEmitter, Input, effect, HostListener } from '@angular/core';
 import { AfterViewInit, ViewChild, ElementRef } from '@angular/core';
-import { Router } from '@angular/router';    
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { SubastasService } from '../../services/subastas.service';
 import { Subasta } from '../../models/subasta.model';
@@ -28,103 +28,65 @@ export class GeneralAuctionsComponent implements OnInit {
   mainTitle: string = '';
   cargandoMas = false;
   hayMas = true;
-  constructor(private lss: LocalSignalsService, private subastaService:SubastasService,private ss: SharedService, private modalService: ModalService,private router: Router, private authService: AuthService, private auctionService: AuctionService ){
-    this.getSubastasSeguidas();  
+  constructor(private lss: LocalSignalsService, private subastaService: SubastasService, private ss: SharedService, private modalService: ModalService, private router: Router, private authService: AuthService, private auctionService: AuctionService) {
+    this.getSubastasSeguidas();
     effect(() => {
-      console.log(this.lss.triggerFunctionID!())
       if (this.lss.triggerToogleFollowedG() > -1) {
-        // this.miFuncion();
-        // this.getSubastasSeguidas();
-        console.log('quitar de seguidos')
-        //this.getSubastasSeguidas();  
-        this.auctionsId = this.auctionsId.length > 0 ? this.auctionsId.filter(x => x !== this.lss.triggerToogleFollowedG()): this.auctionsId;
+        this.auctionsId = this.auctionsId.length > 0 ? this.auctionsId.filter(x => x !== this.lss.triggerToogleFollowedG()) : this.auctionsId;
         this.lss.toogleFollowedIDG(-1);
-        // this.lss.triggerFunction.set(false);
       }
     });
   }
 
   @Input() tipoSeccion = '';
-  
+
   @Output() abrirDetalle = new EventEmitter<{ subasta: Subasta, lista: Subasta[], origen: string }>();
-  
+
   ngOnInit(): void {
-    let tipoSubasta: any = 'todas' ;
-    switch(this.tipoSeccion){
+    let tipoSubasta: any = 'todas';
+    switch (this.tipoSeccion) {
       case 'SubastasPremium':
-          this.mainTitle = 'Xubastas PREMIUM';
-          tipoSubasta = 'premium';
-          break;
+        this.mainTitle = 'Xubastas PREMIUM';
+        tipoSubasta = 'premium';
+        break;
       case 'SubastasExpress':
-          this.mainTitle = 'Xubastas EXPRESS';
-          tipoSubasta = 'porvencer';
-          break;
+        this.mainTitle = 'Xubastas EXPRESS';
+        tipoSubasta = 'porvencer';
+        break;
       default:
-          this.mainTitle = 'Xubastas GENERALES';
-          tipoSubasta = 'todas';
-          break;
-        
+        this.mainTitle = 'Xubastas GENERALES';
+        tipoSubasta = 'todas';
+        break;
+
     }
 
-    this.subastaService.getAuctions(tipoSubasta).subscribe({
-      next: (data) => {
-        this.generales = data.map(subasta => ({
-          ...subasta,
-          tiempoVence: subasta.tiempoVence, // ya viene del backend como string HH:mm:ss
-          vencida: false
-        }));
-        for(let p of this.generales){
-          p.venceSegundos = this.tiempoStringASegundos(p.tiempoVence);
-        }
-        this.setTimer(this.generales);
-        // interval(1000).subscribe(() => {
-        //   const ahora = new Date();
-        
-        //   this.generales.forEach(subasta => {
-        //     subasta.tiempoVence = this.restarUnSegundo(subasta.tiempoVence);
-        
-        //     if (subasta.tiempoVence === '00:00:00' && !subasta.vencida) {
-        //       subasta.vencida = true;
-        
-        //       // Eliminar visualmente después de 1s (permite animación CSS)
-        //       setTimeout(() => {
-        //         this.generales = this.generales.filter(s => s !== subasta);
-        //       }, 1000);
-        //     }
-        //   });
-        // });
-        },
-      error: (error) => {
-        console.error('Error cargando subastas:', error);
-      }    
-    });
+    this.cargarMasSubastas()
   }
-  
-  // verificarSiSiguiendo(): void {
-  //   const idUsuario = Number(this.authService.idUsuario);
-  //   this.subastaService.ConsultarSiSiguiendo(idUsuario, this.subasta.id)
-  //     .subscribe({
-  //       next: res => this.isFollowed = res === true,
-  //       // next: res => this.estaSiguiendo = res === true,
-  //       error: err => console.error('Error seguimiento:', err)
-  //     });
-  // }
 
-  setTimer(litaItems: any[]){
+  setTimer(litaItems: any[]) {
+    if (this.intervalId) {
+      return;
+    }
+
     this.intervalId = setInterval(() => {
-      for(let item of litaItems){
+
+      for (let item of litaItems) {
+
         if (item.venceSegundos > 0) {
           item.venceSegundos--;
         }
+        
       }
+
     }, 1000);
+
   }
 
   tiempoStringASegundos(tiempo: string) {
     const [h, m, s] = tiempo.split(':').map(Number);
     return h * 3600 + m * 60 + s;
   }
-  
+
   // 2. Función para convertir segundos a "hh:mm:ss"
   segundosATiempoString(segundos: number) {
     const h = String(Math.floor(segundos / 3600)).padStart(2, '0');
@@ -133,24 +95,27 @@ export class GeneralAuctionsComponent implements OnInit {
     return `${h}:${m}:${s}`;
   }
 
+  contadorTiempo(tiempo: string) {
+
+  }
+
   abrirModal(subasta: Subasta) {
-    console.log('Subasta seleccionada:', subasta);
     //this.abrirDetalle.emit({ subasta,  origen: 'todas' });
     //this.modalService.abrir(subasta);
     this.router.navigate(['/subasta-detalle', subasta.id, 'Subastas Generales']);
   }
 
-  getSubastasSeguidas(){
+  getSubastasSeguidas() {
     const usuario = this.authService.currentUser();
-      if (usuario) {
-        const idUsuario = usuario.id;
-        this.auctionService.getAuctions(idUsuario).subscribe({
-          next: (data) => {
-            this.auctionsId = data.map(subasta => subasta.id);
-          },
-          error: (error) => {
-            console.error('Error cargando subastas:', error);
-          }
+    if (usuario) {
+      const idUsuario = usuario.id;
+      this.auctionService.getAuctions(idUsuario).subscribe({
+        next: (data) => {
+          this.auctionsId = data.map(subasta => subasta.id);
+        },
+        error: (error) => {
+          console.error('Error cargando subastas:', error);
+        }
       });
     } else {
       console.warn('Usuario no logueado, no se cargan subastas seguidas');
@@ -165,12 +130,10 @@ export class GeneralAuctionsComponent implements OnInit {
   toggleSeguida(idSubasta: number, event: Event): void {
     event.stopPropagation();
     const usuario = this.authService.currentUser();
-    if(usuario){
-       if (this.isSeguida(idSubasta)) {
+    if (usuario) {
+      if (this.isSeguida(idSubasta)) {
         this.subastaService.dejarDeSeguirSubasta(usuario!.id, idSubasta.toString()).subscribe({
           next: (data) => {
-            console.log('resultado seguir subasta');
-            console.log(data);
             this.getSubastasSeguidas();
             this.lss.ejecutarFuncion();
           },
@@ -178,13 +141,10 @@ export class GeneralAuctionsComponent implements OnInit {
             console.error('Error al agregar subasta seguida:', error);
           }
         })
-      // this.auctionsId = this.auctionsId.filter(id => id !== idSubasta);
+        // this.auctionsId = this.auctionsId.filter(id => id !== idSubasta);
       } else {
-        console.log(usuario!.id);
         this.subastaService.seguirSubasta(usuario!.id, idSubasta.toString()).subscribe({
           next: (data) => {
-            console.log('resultado seguir subasta');
-            console.log(data);
             this.getSubastasSeguidas();
             this.lss.ejecutarFuncion();
           },
@@ -195,19 +155,19 @@ export class GeneralAuctionsComponent implements OnInit {
         // this.auctionsId.push(idSubasta);
       }
     }
-   
+
   }
 
   restarUnSegundo(tiempo: string): string {
     const [h, m, s] = tiempo.split(':').map(Number);
     let total = h * 3600 + m * 60 + s - 1;
-  
+
     if (total <= 0) return '00:00:00';
-  
+
     const hh = Math.floor(total / 3600);
     const mm = Math.floor((total % 3600) / 60);
     const ss = total % 60;
-  
+
     return `${this.pad(hh)}:${this.pad(mm)}:${this.pad(ss)}`;
   }
   pad(n: number): string {
@@ -218,19 +178,18 @@ export class GeneralAuctionsComponent implements OnInit {
   //   // console.log('Subasta express seleccionada:', subasta);
   //   this.router.navigate(['/subasta', subasta.id, 'Subastas Premium']);
   // }
-  getDatosSubasta(id: number){
+  getDatosSubasta(id: number) {
     this.loading = true;
     this.subastaService.getAuctionById(id).subscribe({
       next: (subasta) => {
-        let tiempoVence = subasta.tiempoVence?? '00:00:00';
+        let tiempoVence = subasta.tiempoVence ?? '00:00:00';
         let segundos: number, minutos: number, horas: number;
         let _tiempoRestante = tiempoVence.split(':').reduce((acc, time) => (60 * acc) + +time, 0);
-        console.log(_tiempoRestante);
         this.loading = false;
-        if(_tiempoRestante > 0){
+        if (_tiempoRestante > 0) {
           this.router.navigate(['/subasta-detalle', subasta.id, 'Generales']);
         } else {
-          let dataParams = JSON.stringify({ idSubasta: id, tipoUsuario:'comprador'});
+          let dataParams = JSON.stringify({ idSubasta: id, tipoUsuario: 'comprador' });
           let encoded = this.ss.encodeToBase64(dataParams);
           this.router.navigate(['/subasta-terminada', encoded]);
         }
@@ -248,51 +207,54 @@ export class GeneralAuctionsComponent implements OnInit {
 
   cargarMasSubastas() {
 
-  if (this.cargandoMas || !this.hayMas) return;
+    if (this.cargandoMas || !this.hayMas) return;
 
-  this.cargandoMas = true;
+    this.cargandoMas = true;
 
-  this.subastaService.getAuctions('todas').subscribe({
-    next: (respuesta) => {
+    this.subastaService.getAuctions('todas').subscribe({
+      next: (respuesta) => {
 
-      const nuevas = respuesta.filter(
-        x => !this.generales.some(s => s.id === x.id)
-      );
+        const nuevas = respuesta.filter(
+          x => !this.generales.some(s => s.id === x.id)
+        );
 
-      this.generales.push(...nuevas);
+        this.generales.push(...nuevas);
+        // Inicializar temporizador de las nuevas
+        nuevas.forEach(p => {
+          p.venceSegundos = this.tiempoStringASegundos(p.tiempoVence);
+        });
 
-      // Inicializar temporizador de las nuevas
-      nuevas.forEach(p => {
-        p.venceSegundos = this.tiempoStringASegundos(p.tiempoVence);
-      });
+        if (!this.intervalId) {
+          this.setTimer(this.generales)
+        }
 
-      this.cargandoMas = false;
+        this.cargandoMas = false;
 
-      // Si no llegó ninguna nueva, ya no seguir intentando
-      if (nuevas.length === 0) {
-        this.hayMas = false;
+        // Si no llegó ninguna nueva, ya no seguir intentando
+        if (nuevas.length === 0) {
+          this.hayMas = false;
+        }
+      },
+      error: () => {
+        this.cargandoMas = false;
       }
-    },
-    error: () => {
-      this.cargandoMas = false;
+    });
+
+  }
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+
+    const posicion =
+      window.innerHeight + window.scrollY;
+
+    const altura =
+      document.body.offsetHeight;
+
+    if (posicion >= altura - 200) {
+      this.cargarMasSubastas();
     }
-  });
 
-}
-@HostListener('window:scroll', [])
-onScroll(): void {
-
-  const posicion =
-    window.innerHeight + window.scrollY;
-
-  const altura =
-    document.body.offsetHeight;
-
-  if (posicion >= altura - 200) {
-    this.cargarMasSubastas();
   }
 
-}
 
- 
 }
